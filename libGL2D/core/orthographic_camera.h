ad0290203d1e8ee8979fbe56693cfa097008bc9c
eipiequals-1 <olivier.lvannier@gmail.com>
2022-06-23 10:26:39 -0700

    Changed Orthographic to proper Matrix math with an individual view and projection matrix instead of just a projection matrix

---
+++ libGL2D/core/orthographic_camera.h
@@ -0,0 +1,62 @@
+#ifndef _LIBGL2D_CORE_ORTHOCAMERA_H_
+#define _LIBGL2D_CORE_ORTHOCAMERA_H_
+
+#include "libGL2D/physics/math.h"
+
+namespace libgl {
+
+/**
+ * A 2D Orthographic Camera for rendering
+ * Intended Use:
+ *
+ * OrthoCamera cam(width, height, screen_res);
+ * SpriteBatch batch;
+ * cam.RecalculateViewMatrix();
+ * batch.SetViewProjectionMatrix(cam.GetViewProjectionMatrix());
+ * batch.BeginRender();
+ *
+ * . . .
+ *
+ * batch.EndRender();
+ */
+class OrthographicCamera {
+   public:
+	OrthographicCamera(float left, float right, float bottom, float top);
+
+	const glm::vec3& GetPosition() const { return position_; }
+	void SetPosition(const glm::vec3& position) { position_ = position; }
+
+	float GetRotation() const { return rotation_; }
+	void SetRotation(float rotation) { rotation_ = rotation; }
+
+	float GetWidth() const { return width_; }
+	float GetHeight() const { return height_; }
+
+	const glm::mat4& GetProjectionMatrix() const { return projection_matrix_; }
+	const glm::mat4& GetViewMatrix() const { return view_matrix_; }
+	const glm::mat4& GetViewProjectionMatrix() const { return view_projection_matrix_; }
+
+	/**
+	 * Converts the screen/pixel coordianates to world coordinates
+	 * @param vec the coordinates to convert
+	 */
+	glm::vec2 Project(const glm::vec2& vec, const glm::vec2& screen_res) const;
+	/**
+	 * Updates the view projection matrix if position and/or size have changed
+	 *
+	 * IMPORTANT: must be called before rendering if the camera has changed
+	 */
+	void RecalculateViewMatrix();
+
+   private:
+	float width_, height_;
+
+	glm::vec3 position_;
+	glm::mat4 projection_matrix_;
+	glm::mat4 view_matrix_;
+	glm::mat4 view_projection_matrix_;
+	float rotation_;
+};
+}  // namespace libgl
+
+#endif  // _LIBGL2D_CORE_ORTHOCAMERA_H_
