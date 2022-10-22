6b553aafca9a3b110f92c7477758c882d1e0d6ff
eipiequals-1 <olivier.lvannier@gmail.com>
2022-10-22 13:41:08 -0700

    create string split method

--- omega/util/std.h
+++ omega/util/std.h
@@ -1,7 +1,10 @@
 #ifndef OMEGA_UTIL_STD_H
 #define OMEGA_UTIL_STD_H
 
+#include <string.h>
+
 #include <array>
+#include <filesystem>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -29,6 +32,25 @@ using sptr = std::shared_ptr<T>;
 template <typename T, typename... Args>
 constexpr sptr<T> create_sptr(Args &&...args) {
     return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+/**
+ * Splits the string into a vector of substrings using the delimeter
+ * @param str the string to split
+ * @param del the delimeter to split the string with
+ * @return vector<string> of the split string
+ */
+inline std::vector<std::string> split(std::string str, const std::string &del) {
+    std::vector<std::string> ret_vec;
+    char *s = &str[0]; // get pointer to string
+    // start splitting string
+    char *piece = strtok(s, del.c_str());
+    while (piece != nullptr) {
+        ret_vec.push_back(std::string(piece));
+        // get next string token
+        piece = strtok(nullptr, del.c_str());
+    }
+    return ret_vec;
 }
 
 } // namespace omega::util
