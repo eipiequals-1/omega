83bad499f08f050b932a28d48487617c0276352a
eipiequals-1 <olivier.lvannier@gmail.com>
2023-02-19 11:36:07 -0800

    add simply Ray class and ray, block collision

--- omega/util/math.hpp
+++ omega/util/math.hpp
@@ -9,8 +9,12 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/hash.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include <glm/gtx/norm.hpp>
 
 #include <iostream>
+#include <optional>
+#include <tuple>
+#include <functional>
 
 namespace omega::util {
 /**
@@ -20,8 +24,91 @@ enum class Direction {
     LEFT = 0,
     RIGHT = 1,
     UP = 2,
-    DOWN = 3
+    DOWN = 3,
+    FORWARDS = 4,
+    BACKWARDS = 5
 };
+
+inline glm::vec3 intbound(glm::vec3 s, glm::vec3 ds) {
+    glm::vec3 res;
+    for (size_t i = 0; i < 3; i++) {
+        res[i] =
+            (ds[i] > 0 ?
+             (glm::ceil(s[i]) - s[i])
+             : (s[i] - glm::floor(s[i])))
+            / glm::abs(ds[i]);
+    }
+    return res;
+}
+
+// Direct copy of https://github.com/jdah/minecraft-again/blob/master/src/util/ray.hpp
+struct Ray {
+    glm::vec3 origin{0.0f};
+    glm::vec3 direction{0.0f};
+
+    Ray(const glm::vec3& origin, const glm::vec3 &direction) : origin(origin),
+                                                               direction(direction) {}
+
+    std::optional<std::tuple<glm::ivec3, glm::vec3>> intersect_block(std::function<bool(glm::ivec3)> f, float max_distance) {
+        glm::ivec3 d{0};
+        glm::ivec3 p, step;
+        glm::vec3 t_max, t_delta;
+        float radius;
+
+        p = glm::floor(origin);
+        step = glm::sign(direction);
+        t_max = util::intbound(origin, direction);
+        t_delta = glm::vec3(step) / this->direction;
+        radius = max_distance / glm::l2Norm(direction);
+
+        while (true) {
+            if (f(p)) {
+                return std::make_optional(std::make_tuple(p, d));
+            }
+
+            if (t_max.x < t_max.y) {
+                if (t_max.x < t_max.z) {
+                    if (t_max.x > radius) {
+                        break;
+                    }
+
+                    p.x += step.x;
+                    t_max.x += t_delta.x;
+                    d = glm::ivec3(-step.x, 0, 0);
+                } else {
+                    if (t_max.z > radius) {
+                        break;
+                    }
+
+                    p.z += step.z;
+                    t_max.z += t_delta.z;
+                    d = glm::ivec3(0, 0, -step.z);
+                }
+            } else {
+                if (t_max.y < t_max.z) {
+                    if (t_max.y > radius) {
+                        break;
+                    }
+
+                    p.y += step.y;
+                    t_max.y += t_delta.y;
+                    d = glm::ivec3(0, -step.y, 0);
+                } else {
+                    if (t_max.z > radius) {
+                        break;
+                    }
+
+                    p.z += step.z;
+                    t_max.z += t_delta.z;
+                    d = glm::ivec3(0, 0, -step.z);
+                }
+            }
+        }
+
+        return std::nullopt;
+    }
+};
+
 } // namespace omega::util
 
 namespace glm {
@@ -41,7 +128,7 @@ struct rect {
      * Shifts the rectangle by the given vector
      * @param a the amount to shift
      */
-    virtual void translate(const glm::vec2 &a) {
+    void translate(const glm::vec2 &a) {
         x += a.x;
         y += a.y;
     }
@@ -64,7 +151,7 @@ struct rect {
      * @param a the other rect
      * @return if they overlap
      */
-    virtual bool overlaps(const rect<T> &a) const {
+    bool overlaps(const rect<T> &a) const {
         return x < a.x + a.w && x + w > a.x && y < a.y + a.h && y + h > a.y;
     }
 
@@ -73,14 +160,14 @@ struct rect {
      * @param p a vec2 representing the point
      * @return if the point is contained
      */
-    virtual bool point_in_rect(const vec2 &p) const {
+    bool point_in_rect(const vec2 &p) const {
         return x < p.x && p.x < x + w && y < p.y && p.y < y + h;
     }
 
     /**
      * Returns the {string} rectangle data for debugging purposes
      */
-    virtual std::string to_string() const {
+    std::string to_string() const {
         return std::string("rect(") + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(w) + ", " + std::to_string(h) + ")";
     }
 
@@ -88,7 +175,7 @@ struct rect {
      * @param a another rect
      * @return if the position and sizes are exactly the same
      */
-    virtual bool operator==(const rect<T> &a) const {
+    bool operator==(const rect<T> &a) const {
         return a.x == x && a.y == y && a.w == w && a.h == h;
     }
 
@@ -96,7 +183,7 @@ struct rect {
      * @param a another rect
      * @return if the rectangles are not equal
      */
-    virtual bool operator!=(const rect<T> &a) const {
+    bool operator!=(const rect<T> &a) const {
         return !(a == *this);
     }
 
