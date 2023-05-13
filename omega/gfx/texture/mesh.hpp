fd3080324bd804ecc96ac7927d851fcfb0c35393
eipiequals-1 <olivier.lvannier@gmail.com>
2023-05-13 10:28:34 -0700

    commit before new remote repo

--- omega/gfx/texture/mesh.hpp
+++ omega/gfx/texture/mesh.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <span>
 
 #include "omega/gfx/index_buffer.hpp"
 #include "omega/gfx/shader.hpp"
@@ -14,37 +15,15 @@
 
 namespace omega::gfx::texture {
 
-struct MeshVertex {
-    glm::vec3 position;
-    glm::vec3 normal;
-    glm::vec2 tex_coords;
-    glm::vec3 tangent;
-    glm::vec3 bitangent;
-};
-
-struct MeshTexture {
-    uint32_t id;
-    std::string type;
-    std::string path;
-};
-
 class Mesh {
   public:
-    Mesh(
-        const std::vector<MeshVertex>& vertices,
-        const std::vector<uint32_t>& indices,
-        const std::vector<MeshTexture>& textures
-    );
+    Mesh();
 
+    template <typename T>
+    void set_vertices(const std::span<T> *data) {}
     void render(Shader &shader);
 
   private:
-    void setup_mesh();
-
-    std::vector<MeshVertex> vertices;
-    std::vector<uint32_t> indices;
-    std::vector<MeshTexture> textures;
-
     util::uptr<VertexArray> vao = nullptr;
     util::uptr<VertexBuffer> vbo = nullptr;
     util::uptr<IndexBuffer> ibo = nullptr;
